1333430bd16330058de7eb472bb8a55546da8f05
Ryan Carrier <ryan.j.carrier@gmail.com>
2017-03-05 19:43:42 +1000

    added expected tests

---
+++ expected_test.go
@@ -0,0 +1,42 @@
+package cryptoclientgo
+
+import "testing"
+
+func TestGetBest(t *testing.T) {
+	o := getOrders()
+	amt, err := o.getBestBuy(4 * Multiplier)
+	if err != nil {
+		t.Error("Err not nil")
+	}
+	want := (1*1 + 1*2 + 1*2) * Multiplier
+	if amt != want {
+		t.Errorf("Amt:\t%+v\nWant:\t%+v", amt, want)
+	}
+}
+
+func TestGetSell(t *testing.T) {
+	o := getOrders()
+	amt, err := o.getBestSell(4 * Multiplier)
+	if err != nil {
+		t.Error("Err not nil")
+	}
+	want := (5*4 + 1*2) * Multiplier
+	if amt != want {
+		t.Errorf("Amt:\t%+v\nWant:\t%+v", amt, want)
+	}
+}
+
+func getOrders() Orders {
+	o := Orders{
+		{1, 1},
+		{1, 2},
+		{5, 4},
+		{2, 2},
+	}
+
+	for i := range o {
+		o[i].Price *= Multiplier
+		o[i].Volume *= Multiplier
+	}
+	return o
+}
